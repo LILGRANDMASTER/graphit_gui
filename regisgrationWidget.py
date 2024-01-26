04be4a98f22c32981f2eb1923e76e45e7a4f1763
unknown <morojenka111@gmail.com>
2024-01-26 14:58:20 +0300

    registration journal

--- regisgrationWidget.py
+++ regisgrationWidget.py
@@ -1,14 +1,27 @@
 import sys
+import datetime
 from PyQt5.QtWidgets import QWidget
 
 sys.path.insert(1, './ui_files')
 from registrationWindow import Ui_registrationWin
 
 class RegistrationWidget(QWidget):
-    def __init__(self, parent=None):
-        super().__init__(parent=None)
+	def __init__(self, parent=None):
+		super().__init__(parent=None)
 
-        self.ui = Ui_registrationWin()
-        self.ui.setupUi(self)
+		self.ui = Ui_registrationWin()
+		self.ui.setupUi(self)
+        
+		self.ui.login.clicked.connect(self.saveRegistration)
 
+	def saveRegistration(self):
+		
+		today = datetime.datetime.today().strftime('%d.%m.%Y-%H:%M')
+		fio = self.ui.opSecName.text() + ' ' + self.ui.opName.text() + ' ' + self.ui.opProton.text() + ' ' + today + '\n'
+
+		self.ui.login.setStyleSheet('QPushButton {background-color: green;}')
+
+		journal = open('./log/registration.txt', 'a', encoding='utf-8')
+		journal.write(fio)
+		journal.close()
 
--- servoSettingsWidget.py
+++ servoSettingsWidget.py
@@ -1,12 +1,4 @@
 import sys
-#from PyQt5.QtCore import Qt
-#from PyQt5.QtGui import *
-#from PyQt5.QtWidgets import (
-#								QApplication, QVBoxLayout, QWidget,
-#								QLabel, QGridLayout, QPushButton, 
-#								QHBoxLayout, QLineEdit, QComboBox,
-#								QMainWindow, QMenuBar, QMenu, QAction
-#							)
 from PyQt5.QtWidgets import QWidget
 
 sys.path.insert(1, './ui_files')
