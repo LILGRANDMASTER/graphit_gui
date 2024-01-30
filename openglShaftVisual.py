d43b903c6cae406ace076fe33c860b971c97cfb3
unknown <morojenka111@gmail.com>
2024-01-30 11:05:11 +0300

    draw Polygons

--- openglShaftVisual.py
+++ openglShaftVisual.py
@@ -5,6 +5,20 @@ from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 import numpy as np
+import enum
+
+
+
+forms = {
+    'triangle': 3,
+    'sqare': 4,
+    'pentagon': 5,
+    'hexagon': 6,
+    'heptagon': 7,
+    'octagon': 8,
+    'ninesided': 9,
+    'decagon': 10
+}
 
 class OpenGLWidget(QOpenGLWidget):
     def initializeGL(self):
@@ -14,50 +28,40 @@ class OpenGLWidget(QOpenGLWidget):
         glClear(GL_COLOR_BUFFER_BIT)  # Очищаем буфер цвета
         glLoadIdentity()  # Сбрасываем текущую матрицу
 
-        self.draw_octagon(0.5, "red")  # Рисуем внешний восьмиугольник
-        self.draw_octagon(0.3, "green")  # Рисуем внутренний восьмиугольник
+        self.drawPolygon('octagon', 1, [1.0, 1.0, 1.0])  # Рисуем внешний восьмиугольник
+        self.drawPolygon('octagon', 0.5, [0.0, 0.0, 0.0]) #Рисуем внутренний восьмиугольник
+        self.drawHatching()
 
         glFlush()  # Принудительно выводим все команды на выполнение
 
     def resizeGL(self, width, height):
         glViewport(0, 0, width, height)  # Устанавливаем область вывода
 
-    #Восьмиугольник
-    def draw_octagon(self, size, color):
-        if(color == "red"):
-            glColor3f(1.0, 0.0, 0.0)  # Устанавливаем цвет (красный)
-        else:
-            glColor3f(0.0, 1.0, 0.0)
-        glBegin(GL_POLYGON)  # Начинаем рисовать многоугольник
 
-        # Указываем вершины восьмиугольника
-        glVertex2f(-size, size)
-        glVertex2f(-size/2, size*1.5)
-        glVertex2f(size/2, size*1.5)
-        glVertex2f(size, size)
-        glVertex2f(size, -size)
-        glVertex2f(size/2, -size*1.5)
-        glVertex2f(-size/2, -size*1.5)
-        glVertex2f(-size, -size)
+    def drawPolygon(self, type, size, color):
+        glColor3f(color[0], color[1], color[2])  # Устанавливаем цвет рисования (белый)
 
-        glEnd()  # Завершаем рисование многоугольника
-
-    #Десятиугольник
-    def draw_decagon(self):
-        glClear(GL_COLOR_BUFFER_BIT)  # Очищаем экран
-
-        glColor3f(1.0, 1.0, 1.0)  # Устанавливаем цвет рисования (белый)
-
-        # Рисуем 10-угольник
+        # Рисуем многоугольник
         glBegin(GL_POLYGON)
-        for i in range(10):
-            angle = 2 * 3.14159 * i / 10
-            x = 0.5 * np.cos(angle)
-            y = 0.5 * np.sin(angle)
+        for i in range(forms[type]):
+            angle = 2 * 3.14159 * i / forms[type]
+            x = size * np.cos(angle)
+            y = size * np.sin(angle)
             glVertex2f(x, y)
         glEnd()
 
+    def drawHatching(self):
+        glColor3f(0.0, 0.0, 0.0)  # Устанавливаем цвет вертикальных линий (черный)
+        glBegin(GL_LINES)
+        num_lines = 50  # Количество линий
+        for i in range(num_lines):
+            x = -1 + (1.0 / num_lines) * i
+            glVertex2f(x, 1)
+            glVertex2f(x, -1)
+        glEnd()
 
+
+#ДЛЯ ОТЛАДКИ
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
